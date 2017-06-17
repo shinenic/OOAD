76f1053387a6e068a65f4d3b0f57b3a0d39e4e41
KadenzWei <galadiya41@gmail.com>
2017-06-17 17:47:08 +0800

    將比較按鈕移至右下方

--- OOADProject/OOADProject/Scripts/compare.js
+++ OOADProject/OOADProject/Scripts/compare.js
@@ -1,23 +1,29 @@
 ﻿$(function () {
-    //$("#Table_Compare").tablesorter();
-    if ($("#Compare").val() != "")
+    $("#Button_Compare").hide();
+    if ($("#Compare").val() != "") {
         $("#Compare").val($("#Compare").val() + "-");
+        $("#Button_Compare").show();
+    }
     $("input[id*='CheckBox_Car']").change(function () {
         if (this.checked) {
-            $("#Compare").val($("#Compare").val() + this.name+"-");
+            $("#Compare").val($("#Compare").val() + this.name + "-");
         }
         else {
-            $("#Compare").val($("#Compare").val().replace(this.name+"-",''));
+            $("#Compare").val($("#Compare").val().replace(this.name + "-", ''));
         }
+        if ($("#Compare").val() != "") {
+            $("#Button_Compare").show();
+        }
+        else
+            $("#Button_Compare").hide();
     });
     $("#Button_Compare").click(function () {
         $("#Compare").val($("#Compare").val().slice(0, -1));
         count = $("#Compare").val().split("-");
-        if (count.length < 2)
-        {
+        if (count.length < 2) {
             alert("請至少勾選兩台車");
             return false;
         }
-            
+
     });
 })
